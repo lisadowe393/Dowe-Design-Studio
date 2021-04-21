f614391559388b8d7e759041fe1eaf9137cc4769
Robert Clarkson <robert.ian.clarkson@gmail.com>
2021-04-21 16:41:31 -0700

    Creme Done

--- scripts/cremephraiche.js
+++ scripts/cremephraiche.js
@@ -1,36 +1,36 @@
 
-  var buttons = document.querySelectorAll (".dot")
-  buttons.forEach (function(btn, i){
-    btn.addEventListener("click", function(){
-      gsap.to(".slider", {
-        x:-i *800,
-        overwrite:"auto"
-    })
+//   var buttons = document.querySelectorAll (".dot")
+//   buttons.forEach (function(btn, i){
+//     btn.addEventListener("click", function(){
+//       gsap.to(".slider", {
+//         x:-i *800,
+//         overwrite:"auto"
+//     })
 
-})
-  })
+// })
+//   })
 
 
 
   
 
-gsap.to('#animals, #animals1', {
-    xPercent: 100,
-    repeat: -1,
-    ease: "none",
-    duration: 35
+// gsap.to('#animals, #animals1', {
+//     xPercent: 100,
+//     repeat: -1,
+//     ease: "none",
+//     duration: 35
 
-})
+// })
 
 
-var animation = gsap.timeline({defaults:{ duration: 5, ease:'none'}})
-animation.to('#jeep', {
-  x: 1600,
-  repeat: -1,
-  ease: "none",
-  duration: 6
+// var animation = gsap.timeline({defaults:{ duration: 5, ease:'none'}})
+// animation.to('#jeep', {
+//   x: 1600,
+//   repeat: -1,
+//   ease: "none",
+//   duration: 6
 
-})
+// })
 
 
 
