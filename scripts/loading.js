a564f3c49e63c163adf72cec727b3566b93c091e
Robert Clarkson <robert.ian.clarkson@gmail.com>
2021-05-02 23:05:49 -0700

    Good animation check BP

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,10 +1,14 @@
 document.onreadystatechange = function () {
   var state = document.readyState
   if (state == 'complete') {
-    document.getElementById('interactive');
-    document.getElementById('load').style.visibility="hidden";
-    document.getElementById('content').style.animation="fadeIn 2s";
-    document.getElementById('content').style.visibility="visible";
+    document.getElementById('load').style.animation="fadeOut .5s";
+    setTimeout(function(){ 
+      document.getElementById('interactive');
+      document.getElementById('load').style.visibility="hidden";
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
+      document.getElementById('content').style.animation="fadeIn .5s";
+      document.getElementById('content').style.visibility="visible";
+    }, 500);
   } else {
     document.getElementById('load').style.visibility="visible";
     document.getElementById('content').style.visibility="hidden";
